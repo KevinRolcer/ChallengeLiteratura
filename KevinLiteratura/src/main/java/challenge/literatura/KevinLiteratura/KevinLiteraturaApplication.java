22818a800a8ad3f365c3fde10053883b4bf0c1f9
Kevin Roldan <134951931+KevinRolcer@users.noreply.github.com>
2024-06-29 09:15:23 -0600

    Add files via upload

---
+++ KevinLiteratura/src/main/java/challenge/literatura/KevinLiteratura/KevinLiteraturaApplication.java
@@ -0,0 +1,13 @@
+package challenge.literatura.KevinLiteratura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinLiteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KevinLiteraturaApplication.class, args);
+	}
+
+}
---
+++ KevinLiteratura/src/test/java/challenge/literatura/KevinLiteratura/KevinLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.literatura.KevinLiteratura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KevinLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
